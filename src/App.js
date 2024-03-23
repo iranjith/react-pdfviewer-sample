659c7dfde86f9c2101c08be854690b03b984ca1a
iranjith <rgranjith89@gmail.com>
2024-03-23 22:27:45 +0530

    loop through search text collection and highlight

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import "./App.css";
 const App = () => {
   const viewer = useRef(null);
 
+  var dummyData = [
+    {
+      searchText:
+        "Additionally, custom features will have to be supported and maintained long-term, creating an additional ongoing opportunity cost: commit-ted resources will be less-available to work on other projects.",
+    },
+    {
+      searchText:
+        "Another mistake is where organizations select a basic library to save money with the assumption that they can build anything needed on top.",
+    },
+  ];
+
   useEffect(() => {
-    // If you prefer to use the Iframe implementation, you can replace this line with: WebViewer.Iframe(...)
     WebViewer.WebComponent(
       {
         path: "/webviewer/lib",
@@ -19,50 +29,61 @@ const App = () => {
         instance.Core;
 
       documentViewer.setSearchHighlightColors({
-        // setSearchHighlightColors accepts both Annotations.Color objects or 'rgba' strings
         searchResult: new Annotations.Color(0, 0, 255, 0.5),
         activeSearchResult: "rgba(0, 255, 0, 0.5)",
       });
 
       documentViewer.addEventListener("documentLoaded", () => {
         console.log("document loaded");
-        const textToSearch = `Another mistake is where organizations select a basic library to save money with the assumption that they can build anything needed on top.`; // replace with the text you want to search for
-        const modeOptions = Search.Mode.PAGE_STOP | Search.Mode.HIGHLIGHT;
+        //const textToSearch = `Another mistake is where organizations select a basic library to save money with the assumption that they can build anything needed on top.`; // replace with the text you want to search for
 
-        console.log("searching for text", textToSearch);
+        dummyData.forEach((data) => {
+          const textToSearch = data.searchText;
+          console.log("searching for text: ", textToSearch);
+          const modeOptions = Search.Mode.PAGE_STOP | Search.Mode.HIGHLIGHT;
+          
+          const searchOptions = {
+            fullSearch: true,
+            onResult: async (searchResults) =>
+              await annotateSearchResult(searchResults),
+            startPage: 1,
+            endPage: 9,
+          };
 
-        const searchOptions = {
-          fullSearch: true,
-          onResult: (searchResults) => {
-            console.log("on result", searchResults);
+          documentViewer
+            .textSearchInit(textToSearch, modeOptions, searchOptions)
+            .then(async () => {
+              await console.log("search initialized");
+            });
 
+          const annotateSearchResult = (searchResults) => {
+            console.log("annotate search result: ", searchResults)
             if (searchResults.resultCode === Search.ResultCode.FOUND) {
-              const textQuad = searchResults.quads[0].getPoints(); // getPoints will return Quad objects
-              // now that we have the result Quads, it's possible to highlight text or create annotations on top of the text
+              const quadsArray = searchResults.quads;
               const firstResult = searchResults;
               const pageNumber = firstResult.pageNum;
 
-              const rectangleAnnot = new Annotations.RectangleAnnotation({
-                PageNumber: pageNumber,
-                X: textQuad.x1,
-                Y: textQuad.y1,
-                Width: textQuad.x3 - textQuad.x1,
-                Height: textQuad.y3 - textQuad.y1,
-              });
+              for (let i = 0; i < quadsArray.length; i++) {
+                const quadElement = quadsArray[i];
+                const textQuad = quadElement.getPoints();
 
-              annotationManager.addAnnotation(rectangleAnnot);
-              annotationManager.redrawAnnotation(rectangleAnnot);
+                const rectangleAnnot = new Annotations.RectangleAnnotation({
+                  PageNumber: pageNumber,
+                  X: textQuad.x1,
+                  Y: textQuad.y1,
+                  Width: textQuad.x3 - textQuad.x1,
+                  Height: textQuad.y3 - textQuad.y1,
+                  StrokeColor: new Annotations.Color(0, 255, 0, 1), // green
+                  StrokeThickness: 2,
+                  Dashes: [3, 2],
+                });
+
+                annotationManager.addAnnotation(rectangleAnnot);
+                annotationManager.redrawAnnotation(rectangleAnnot);
+              }
             }
-          },
-          startPage: 1,
-          endPage: 9,
-        };
-
-        documentViewer
-          .textSearchInit(textToSearch, modeOptions, searchOptions)
-          .then(() => {
-            console.log("search initialized");
-          });
+          };
+        });
       });
     });
   }, []);
